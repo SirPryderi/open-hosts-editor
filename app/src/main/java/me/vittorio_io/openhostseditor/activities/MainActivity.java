cd1b44f74e4cb21b675def419a56bc6a79454206
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-07-23 00:16:11 +0200

    Floating Action Button auto hides on scroll

--- app/src/main/java/me/vittorio_io/openhostseditor/activities/MainActivity.java
+++ app/src/main/java/me/vittorio_io/openhostseditor/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -15,6 +16,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,6 +25,7 @@ import java.util.List;
 
 import me.vittorio_io.openhostseditor.R;
 import me.vittorio_io.openhostseditor.fragments.hostrule.Fragment;
+import me.vittorio_io.openhostseditor.fragments.hostrule.HidingScrollListener;
 import me.vittorio_io.openhostseditor.fragments.hostrule.RecyclerViewAdapter;
 import me.vittorio_io.openhostseditor.model.HostRule;
 import me.vittorio_io.openhostseditor.model.HostsManager;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int WRITE_BACKUP_ACTION = 0;
     public static List<HostRule> rules;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +80,20 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView listView = (RecyclerView) findViewById(R.id.list);
 
         listView.swapAdapter(listViewAdapter, true);
+
+        // source: https://stackoverflow.com/a/39145431
+        listView.setOnScrollListener(new HidingScrollListener() {
+            @Override
+            public void onHide() {
+                CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+                fab.animate().translationY(fab.getHeight() + lp.bottomMargin).setInterpolator(new AccelerateInterpolator(2)).start();
+            }
+
+            @Override
+            public void onShow() {
+                fab.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
+            }
+        });
     }
 
     private void openEditRuleActivity(int index){
