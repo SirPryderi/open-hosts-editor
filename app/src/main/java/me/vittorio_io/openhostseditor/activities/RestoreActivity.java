6162892375b9a3424561cbafdc3a1cd7f3421ab2
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-07-26 10:00:31 +0200

    Fixed permission issue in the restore activity

--- app/src/main/java/me/vittorio_io/openhostseditor/activities/RestoreActivity.java
+++ app/src/main/java/me/vittorio_io/openhostseditor/activities/RestoreActivity.java
@@ -1,6 +1,9 @@
 package me.vittorio_io.openhostseditor.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -13,18 +16,14 @@ import me.vittorio_io.openhostseditor.model.HostsManager;
 
 public class RestoreActivity extends BaseActivity {
 
+    public static final int ACTION_LOAD_BACKUPS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restore);
 
-        RecyclerView viewById = (RecyclerView) findViewById(R.id.backup_list);
-
-        try {
-            viewById.setAdapter(iCanHazAdapter());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        getPermissionAndExecute(Manifest.permission.WRITE_EXTERNAL_STORAGE, ACTION_LOAD_BACKUPS);
     }
 
     private RecyclerViewAdapter iCanHazAdapter() {
@@ -35,7 +34,7 @@ public class RestoreActivity extends BaseActivity {
                     case 0: // row pressed
 
                         return;
-                    case 1: // restore pressed
+                    case ACTION_LOAD_BACKUPS: // restore pressed
                         HostsManager.writeFromFile(item);
                         return;
                     case 2: // delete pressed
@@ -52,5 +51,35 @@ public class RestoreActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case ACTION_LOAD_BACKUPS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    RecyclerView viewById = (RecyclerView) findViewById(R.id.backup_list);
+
+                    try {
+                        viewById.setAdapter(iCanHazAdapter());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    haveASnack("Permission denied. Nothing to do here.");
+                    finish();
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
