b8c9ac8949af70b6d4de5948720a347ba69ac06c
Vittorio Iocolano <pryderi.mail@gmail.com>
2017-07-27 10:41:17 +0200

    Updated about page

--- app/src/main/java/me/vittorio_io/openhostseditor/activities/AboutActivity.java
+++ app/src/main/java/me/vittorio_io/openhostseditor/activities/AboutActivity.java
@@ -1,8 +1,6 @@
 package me.vittorio_io.openhostseditor.activities;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
@@ -18,24 +16,17 @@ public class AboutActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        simulateDayNight(/* DAY */ 3);
-        Element adsElement = new Element();
-        adsElement.setTitle("Advertise with us");
 
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setImage(R.mipmap.ic_launcher)
-                .setDescription("Free and open source application to edit the /etc/hosts file for android devices. Root required.")
-                .addItem(new Element().setTitle("Version 6.2").setValue("Sono buttana"))
-                .addItem(adsElement)
+                .setDescription("Free and open source application to edit the /etc/hosts file for android devices.\n[Root required]")
+                .addItem(new Element().setTitle("Version 0.10.0-alpha").setGravity(Gravity.CENTER))
                 .addGroup("Connect with us")
-                .addEmail("pryderi.mail@gmail.com")
-                .addWebsite("https://github.com/SirPryderi/OpenHostsEditor", "Repository")
+                .addGitHub("SirPryderi/OpenHostsEditor", "GitHub Repository")
+                .addPlayStore("me.vittorio_io.openhostseditor")
                 .addWebsite("http://vittorio-io.me/", "Developer Website")
-                .addPlayStore("com.ideashower.readitlater.pro")
-                .addInstagram("medyo80")
-                .addGitHub("SirPryderi")
-                .addGitHub("SirPryderi", "Astrarium")
+                .addEmail("pryderi.mail@gmail.com")
                 .addItem(getCopyRightsElement())
                 .create();
 
@@ -45,37 +36,16 @@ public class AboutActivity extends BaseActivity {
 
     Element getCopyRightsElement() {
         Element copyRightsElement = new Element();
-        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        final String copyrights = String.format(getString(R.string.copy_right) + " " + getString(R.string.dev_name), Calendar.getInstance().get(Calendar.YEAR));
+        final String copyrightNotice = "Licensed under the Apache License, Version 2.0";
         copyRightsElement.setTitle(copyrights);
-        copyRightsElement.setIconDrawable(R.drawable.ic_add_white_24dp);
-        copyRightsElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_item_icon_color);
-        copyRightsElement.setIconNightTint(android.R.color.white);
         copyRightsElement.setGravity(Gravity.CENTER);
         copyRightsElement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AboutActivity.this, copyrights, Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutActivity.this, copyrightNotice, Toast.LENGTH_SHORT).show();
             }
         });
         return copyRightsElement;
-    }
-
-    void simulateDayNight(int currentSetting) {
-        final int DAY = 0;
-        final int NIGHT = 1;
-        final int FOLLOW_SYSTEM = 3;
-
-        int currentNightMode = getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK;
-        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_NO);
-        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_YES);
-        } else if (currentSetting == FOLLOW_SYSTEM) {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        }
     }
 }
